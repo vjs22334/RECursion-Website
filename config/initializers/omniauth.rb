eebe7f9197c2df555c107bfd42a9bc5a2980b2cb
vjs2234 <jeyasurya.v@gmail.com>
2018-12-07 23:31:00 +0530

    changed oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-	provider :facebook, '456530898208061', 'e5d98b57081009a3b4737740d83a2b15', image_size: 'large', scope: 'email'
-	provider :github, '10afe6d2728eddcb84cd', '9b53e91c1d287dc0216f6e1345d4326a922547d1'
+	provider :facebook, '2301278820118989', '9af440eb6e7abb61c00e0ed3b3bc3f08', image_size: 'large', scope: 'email'
+	provider :github, '3f95a927b2d6744a2c28', 'd40c884569ec1d052c32e747a115526858146a05'
 	OmniAuth.config.logger = Rails.logger
-	provider :google_oauth2, '587144852609-oqfflmk4i0fdtl04944ccq3n6u981okf.apps.googleusercontent.com', 'oZ3MIBVrhG-070yVUcsbn84O', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+	provider :google_oauth2, '240996908487-o3toldjrdtgi1f0qu3obdr4ie95mjd6q.apps.googleusercontent.com', 'OwHqRt1wbKpDWMZ2l3Dy9Bgh', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
